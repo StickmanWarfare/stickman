305b7b1bf3d40edc74359687187fb12aa49beb40
Farkas Zoltan <farkas.zoltan@straxus.hu>
2020-01-05 20:23:22 +0100

    Update hardcoded major version

--- src/Stickman.dpr
+++ src/Stickman.dpr
@@ -16280,7 +16280,7 @@ begin //                 BEGIIIN
 
     assignfile(logfile, 'log.txt');
     rewrite(logfile);
-    writeln(logfile, 'Stickman Warfare v2.' + inttostr((PROG_VER div 1000) mod 100) + '.' + inttostr((PROG_VER div 10) mod 100) + ' (' + inttohex(-exe_checksum, 8) + '). Log file.');
+    writeln(logfile, 'Stickman Warfare v3.' + inttostr((PROG_VER div 1000) mod 100) + '.' + inttostr((PROG_VER div 10) mod 100) + ' (' + inttohex(-exe_checksum, 8) + '). Log file.');
     writeln(logfile, '---------------------------------------');
 
     writeln(logfile, 'Game started at:', formatdatetime('yyyy.mm.dd/hh:nn:ss', date + time));
